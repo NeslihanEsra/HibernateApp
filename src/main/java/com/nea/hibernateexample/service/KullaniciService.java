a447d5d39716c23e73c8038efd5bb18e953d2d80
neslihanesra <neslihanesra@yahoo.com>
2018-02-08 09:15:22 +0300

    init

--- src/main/java/com/nea/hibernateexample/service/KullaniciService.java
+++ src/main/java/com/nea/hibernateexample/service/KullaniciService.java
@@ -5,24 +5,25 @@
  */
 package com.nea.hibernateexample.service;
 
-import com.nea.hibernateexample.db.BaseService;
 import com.nea.hibernateexample.db.IService;
 import com.nea.hibernateexample.db.MyHibernateUtil;
 import com.nea.hibernateexample.model.Kullanici;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author NEA
  */
-public class KullaniciService implements IService<Kullanici>{
+public class KullaniciService implements IService<Kullanici> {
 
     @Override
     public Kullanici insert(Kullanici entity) {
-         return (Kullanici) baseService.insert(entity);
+        return (Kullanici) baseService.insert(entity);
     }
 
     @Override
@@ -53,8 +54,21 @@ public class KullaniciService implements IService<Kullanici>{
         criteria.add(Restrictions.eq("sifre", sifre));
         return (List<Kullanici>) criteria.uniqueResult();
     }
-    
 
+    //Restriction işlemleri
+//    public  List<Kullanici> getRestriction(){
+//        Session session = MyHibernateUtil.getSessionFactory().openSession();        
+//        Criteria criteria = session.createCriteria(Restrictions.and(Restrictions.eq("ad", ad),Restrictions.eq("soyad", soyad)));
+//        Criteria criteria = session.createCriteria(Restrictions.ilike("ad", ad, MatchMode.ANYWHERE));
+//        Criteria c = session.createCriteria(Restrictions.ge("id", 100));
+//        return criteria.list();
+//    }
     
-
+    //sql oluşturma
+    // Criteria criteria = session.createCriteria(Restrictions.sqlRestriction("id>10 and ad='ali'"));
+    // projection  
+    // criteria.setProjection(Projections.max("id"));
+    // pagination   
+    // criteria.setFirstResult(20);
+    // criteria.setMaxResults(10);
 }
